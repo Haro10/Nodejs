8f845916b55b5cb0df687f41903f3be4a8acc89d
Ha Kieu Minh <minhha19122014@gmail.com>
2019-01-14 01:38:15 +0900

    updated code

--- RestfulServices/express-advance-topic/index.js
+++ RestfulServices/express-advance-topic/index.js
@@ -4,9 +4,20 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 const morgan = require('morgan');
+const config = require('config');
+const courses = require('./routes/courses');
+const home = require('./routes/home');
 
-console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
-console.log(`app: ${app.get('env')}`);
+//Often use only a debug in a file -> watch below
+//const startupDebugger =  require('debug')('app:startup');
+//const dbDebugger =  require('debug')('app:db');
+
+//
+const debug = require('debug')('app:startup');
+
+
+// console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
+// console.log(`app: ${app.get('env')}`);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); //key=value&key=value
@@ -18,133 +29,32 @@ app.use(logger);
 
 app.use(helmet());
 
+/*use all url having the format api/courses 
+with courses module we loaded above const courses = require('./courses'); */
+
+app.use('/api/courses', courses); 
+app.use('/home', home);
+
+
+app.set('view engine', 'pug');
+app.set('views', './views'); //set location of template
+
+//configuration
+console.log('Application Name: ' + config.get('name'));
+console.log('Mail Server: ' + config.get('mail.host'));
+console.log('Mail Password: ' + config.get('mail.password'));
+
 if (app.get('env') === 'development') {
     //morgan log a http request - and also can write it to a file
     app.use(morgan('tiny'));
     //console.log('morgan enable.')
-}
 
-// app.use(function(req, res, next){
-//     console.log('Authenticating...');
-//    next();
-// });
+      //using the debug library
+    //   startupDebugger('Morgan enabled...');
+    //   dbDebugger('Connect to database');
 
-const courses = [{
-        id: 1,
-        name: 'course1'
-    },
-    {
-        id: 2,
-        name: 'course2'
-    },
-    {
-        id: 3,
-        name: 'course3'
-    },
-];
-
-app.get('/', (req, res) => {
-    res.send('I will pass everything!!!');
-});
-
-app.get('/api/courses', (req, res) => {
-    res.send(courses);
-});
-
-app.get('/api/courses/:id', (req, res) => {
-    // res.send(req.params.id);
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) return res.status(404).send('The course with the given ID was not found');
-    res.send(course);
-});
-
-app.get('/api/posts/:year/:month', (req, res) => {
-    res.send(req.params);
-});
-
-
-//POST
-
-app.post('/api/courses', (req, res) => {
-    // const {
-    //     error
-    // } = validateCourse(req.body); // error
-
-    // if (error) {
-    //     //400 bad request
-    //     res.status(400).send(error.details[0].message);
-    //     return;
-    // }
-
-    const course = {
-        id: courses.length + 1,
-        name: req.body.name
-    };
-
-    courses.push(course);
-    res.send(course);
-});
-
-//PUT
-app.put('/api/courses/:id', (req, res) => {
-    //Look up the course
-    // If not existing. return 404
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) return res.status(404).send('The course with the given ID was not found');
-
-    // //Validate
-    // //If invalid, return 400 - Bad request
-    // const schema = {
-    //   name: Joi.string().min(3).required()
-    //   };
-    // const result = Joi.validate(req.body, schema);
-
-    // const result = validateCourse(req.body);
-    const {
-        error
-    } = validateCourse(req.body); // error
-
-    if (error) {
-        //400 bad request
-        res.status(400).send(error.details[0].message);
-        return;
-    }
-
-    //Update course
-    course.name = req.body.name;
-
-    //Return the updated course
-    res.send(course);
-
-});
-
-//DELETE
-app.delete('/api/course/:id', (req, res) => {
-    //Look up the course
-    // not existing -> 404 error
-    // const course = courses.find(c => c.id === parseInt(req.params.id));
-    // if(!course){
-    //   res.status(404).send('The course with given Id is not existing');
-    // }
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) res.status(404).send('The course with the given ID was not found');
-
-    res.send("run over here");
-
-    //having that -> delete that
-    const index = courses.indexOf(course);
-    courses.splice(index, 1);
-
-    //Return the same course
-    res.send(courses);
-});
-
-
-function validateCourse(course) {
-    const schema = {
-        name: Joi.string().min(3).required()
-    };
-    return Joi.validate(course, schema);
+     //should use only a debug on a file
+       debug('use only the debug in the file');
 }
 
 ///get the parametters
@@ -157,6 +67,14 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`It is Hakm2 ${port}...`);
 });
+
+
+// app.use(function(req, res, next){
+//     console.log('Authenticating...');
+//    next();
+// });
+
+
 
 
 //HaKM do that
---
+++ RestfulServices/express-advance-topic/routes/home.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    // res.send('I will pass everything!!!');
+    res.render('index', {
+        title: 'My express router',
+        message: 'Hello pug'
+    }); //use pug template engine
+});
+
+module.exports = router;  
